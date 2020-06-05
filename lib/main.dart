a935fffbd36786ee8781fee51c85fc5c8cbd60f7
abdelrahmanbonna <abdobonna@yahoo.com>
2020-06-05 16:26:05 +0200

    bzbat el files

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:napoleon/screens/home.dart';
+import 'package:napoleon/services/UserData.dart';
+import 'package:napoleon/services/WordsData.dart';
+import 'package:napoleon/utilities/constants.dart';
+import 'package:provider/provider.dart';
 
-import 'screens/splash_Screen.dart';
+import 'screens/Login.dart';
 
 void main() {
-  runApp(LoadingScreen());
+  runApp(Main());
+}
+
+class Main extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(value: UserData()),
+        ChangeNotifierProvider.value(value: WordsData()),
+      ],
+      child: MaterialApp(
+        theme: theme,
+        debugShowCheckedModeBanner: false,
+        routes: {
+          Home.id: (context) => Home(),
+          Login.id: (context) => Login(),
+        },
+        initialRoute: Login.id,
+      ),
+    );
+  }
 }
---
+++ lib/utilities/constants.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+final theme = ThemeData(
+  primaryColor: const Color(0xff3bacdb),
+  accentColor: const Color(0xffef4c66),
+  splashColor: const Color(0xffffffff),
+  textTheme: TextTheme(
+    headline1: TextStyle(fontSize: 24, letterSpacing: 0),
+    headline2: TextStyle(fontSize: 20, letterSpacing: 0.25),
+    bodyText1: TextStyle(fontSize: 16, letterSpacing: 0.5),
+    bodyText2: TextStyle(fontSize: 14, letterSpacing: 0.25),
+    subtitle1: TextStyle(fontSize: 16, letterSpacing: 0.15),
+    subtitle2: TextStyle(fontSize: 14, letterSpacing: 0.1),
+    button: TextStyle(fontSize: 14, letterSpacing: 1.25),
+    caption: TextStyle(fontSize: 12, letterSpacing: 0.4),
+    overline: TextStyle(fontSize: 12, letterSpacing: 2),
+  ),
+);
